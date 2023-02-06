477bb246aa277af175d137b5a98d89962f3704eb
Sean Doyle <sdoylelambda@gmail.com>
2023-02-06 10:36:25 -0500

    Basic navbar with links.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'
+
+import Home from './components/Home'
+import About from './components/About'
+import Contact from './components/Contact'
+import Menu from './components/Menu'
+import Navbar from './components/Navbar'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/contact' element={<Contact />} />
+        </Routes>
+        <Navbar />
+      </Router>
+    </>
+  )
 }
 
-export default App;
+export default App
